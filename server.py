12de3ce448b05924d2ad3bcc41ad4a6c7e70c268
Rudransh Sharma <67827010+rudranshsharma123@users.noreply.github.com>
2021-09-28 19:33:09 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify, make_response
+
+from werkzeug.serving import WSGIRequestHandler
+
+app = Flask(__name__)
+
+@app.route('/') # this is the home page route
+def hello_world(): # this is the home page function that generates the page code
+    return "Hello world!"
+
+if __name__ == '__main__':
+  WSGIRequestHandler.protocol_version = "HTTP/1.1"
+  app.run(host='0.0.0.0', port=12345)  
+
+
+
